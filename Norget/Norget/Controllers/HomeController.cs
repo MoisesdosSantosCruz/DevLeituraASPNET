17f82bad52abb72dbac8dad701efb8a94e196837
MathDamas <matheusdormecedo@gmail.com>
2024-11-07 12:19:48 -0300

    Adição de página principal funcionando

--- Norget/Norget/Controllers/HomeController.cs
+++ Norget/Norget/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Norget.Controllers
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
-        {
+        { 
             _logger = logger;
         }
 
